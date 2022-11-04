6b0e1d85b88587ab65fe259addb473291c37d3c6
Siddharth Singh <me@shantaram.xyz>
2022-11-04 23:56:14 +0530

    create get_stdout

---
+++ src/util.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+
+pub fn get_stdout(mut cmd: Command) -> String {
+    let oup = cmd.output();
+    let mut res = String::from("ERROR");
+
+    if let Ok(val) = oup {
+        if val.status.success() {
+            res = String::from_utf8(val.stdout)
+                .unwrap_or_else(|_| String::from("ERROR"))
+                .trim()
+                .to_string();
+        }
+    };
+
+    res
+}
